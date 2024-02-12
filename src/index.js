8a1003a2b151ea5f76c2bceb259c7052758721aa
YonugJones <peterckerfoot@gmail.com>
2024-02-12 12:15:29 -0600

    added console log statements and included example of each class method being called

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import Node from "./nodes";
 import LinkedList from "./linked_lists";
 
-console.log(Node);
+// examples of each method shown in console
 const listOne = new LinkedList('List One');
-console.log(listOne);
+listOne.find('New Node');
 listOne.append('Node 1');
-console.log(listOne);
 listOne.append('Node 2');
-console.log(listOne);
+listOne.prepend('New Head');
+listOne.toString();
+listOne.size();
+listOne.tail();
+listOne.at(1);
+listOne.pop();
+listOne.toString();
+listOne.pop();
+listOne.toString();
+listOne.contains('Node 2');
+listOne.contains('New Head');
+listOne.headFunc();
+listOne.append('New Node');
+listOne.find('New Node');
--- src/linked_lists.js
+++ src/linked_lists.js
@@ -8,6 +8,7 @@ export default class LinkedList {
     append(value) {
         if (this.head === null) {
             this.head = new Node(value);
+            console.log(`${value} has been appended to empty linked list.`);
             return;
         }
         let current = this.head;
@@ -15,60 +16,79 @@ export default class LinkedList {
             current = current.next;
         }
         current.next = new Node(value);
+        console.log(`${value} has been appended to linked list.`);
     }
 
     prepend(value) {
         if (this.head === null) {
             this.head = new Node(value);
+            console.log(`${value} has been prepended to empty linked list.`);
         } else {
             const oldHead = this.head;
             this.head = new Node(value, oldHead);
+            console.log(`${value} has been prepended to linked list.`)
         }
     }
 
     size() {
-        if (this.head === null) return 0;
+        if (this.head === null) {
+            console.log('0');
+            return 
+        };
         let count = 1;
         let current = this.head;
         while (current.next !== null) {
             count += 1;
             current = current.next;
         }
-        return count;
+        console.log(`List size: ${count}`);
     }
 
-    head() {
-        if (this.head === null) return 'No Head';
-        return this.head;
+    headFunc() {
+        if (this.head === null) {
+            console.log('No Head in linked list');
+            return;
+    };
+        console.log(`Head node: ${this.head.value}`);
     }
 
     tail() {
-        if (this.head === null) return 'No Tail';
+        if (this.head === null) {
+            console.log('No Tail');
+            return;
+        };
         let tail = this.head;
         while (tail.next !== null) {
             tail = tail.next;
         }
-        return tail;
+        console.log(`Tail Node: ${tail.value}`);
     }
 
     at(index) {
-        if (this.head === null) return null;
+        if (this.head === null) {
+            console.log(null);
+            return;
+        };
         let currentNode = this.head;
         let counter = 0;
         while (counter < index) {
             currentNode = currentNode.next;
-            if (currentNode === null) return null;
+            if (currentNode === null) {
+                console.log(null)
+            };
             counter += 1;
         }
-        return currentNode;
+        console.log(`Node: ${currentNode.value} is at index: ${index}`);
     }
 
     pop() {
-        if (this.head === null) return null;
+        if (this.head === null) {
+            console.log('Cannot pop node. List is empty')
+        };
         if (this.head.next === null) {
             const popHead = this.head;
             this.head = null;
-            return popHead;
+            console.log(`Head node: ${popHead} has been popped`);
         }
         let current = this.head;
         while (current.next.next !== null) {
@@ -76,28 +96,57 @@ export default class LinkedList {
         }
         const popTail = current.next;
         current.next = null;
-        return popTail;
+        console.log(`Node: ${popTail.value} has been popped`);
     }
 
     contains(value) {
-        if (this.head === null) return false;
+        if (this.head === null) {
+            console.log(`False: List does not contain ${value}`);
+            return false;
+        };
         let current = this.head;
-        while (current.next !== null) {
-            if (current.value === value) return true;
+        while (current !== null) {
+            if (current.value === value) {
+                console.log(`True: List contains ${value}`);
+                return true;
+            };
             current = current.next;
         }
+        console.log(`False: List does not contain ${value}`);
         return false;
     }
 
     find(value) {
-        if (this.head === null) return null;
+        if (this.head === null) {
+            console.log('Cannot find node. The list is empty');
+            return null
+        };
         let currentNode = this.head;
         let index = 0;
         while(currentNode !== null) {
-            if (currentNode.value === value) return index;
+            if (currentNode.value === value) {
+                console.log(`Node: ${value} is at index: ${index}`);
+                return true;
+            };
             index += 1;
             currentNode = currentNode.next;
         }
+        console.log('Cannot find node.');
         return null;
+    }
+
+    toString() {
+        if (this.head === null) {
+            console.log('List is empty');
+            return;
+        }
+        let currentNode = this.head;
+        let string = `( ${currentNode.value} )`;
+        while (currentNode.next !== null) {
+            string += ` -> ( ${currentNode.next.value} )`;
+            currentNode = currentNode.next;
+        }
+        string += ` -> null`;
+        console.log(string);
     }
 }
